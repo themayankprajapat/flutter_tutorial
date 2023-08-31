df15c0c66c0e964936f439dc0552375c257a36c8
mayank <prajapatmayank2905@gmail.com>
2023-08-31 09:47:16 +0530

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "my first app",
+      title: "Flutter Tutorial",
       theme: ThemeData(
           useMaterial3: true,
           appBarTheme: const AppBarTheme(
